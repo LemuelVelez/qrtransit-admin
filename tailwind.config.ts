d5e28981395f424902b9b23f5887e84d3c87a60d
lemuelvelez <pluklem07@gmail.com>
2025-03-31 20:51:20 +0800

    fix: tailwind issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss"
+import type { PluginAPI } from "tailwindcss/types/config"
 import tailwindcssAnimate from "tailwindcss-animate"
 
 const config: Config = {
@@ -73,15 +74,77 @@ const config: Config = {
         spin: {
           to: { transform: "rotate(360deg)" },
         },
+        "pulse-emerald": {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0.5" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         spin: "spin 1s linear infinite",
+        "pulse-emerald": "pulse-emerald 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
     },
   },
-  plugins: [tailwindcssAnimate],
+  plugins: [
+    tailwindcssAnimate,
+    ({ addUtilities }: PluginAPI) => {
+      addUtilities({
+        ".bg-background": {
+          "background-color": "hsl(var(--background))",
+        },
+        ".bg-foreground": {
+          "background-color": "hsl(var(--foreground))",
+        },
+        ".bg-card": {
+          "background-color": "hsl(var(--card))",
+        },
+        ".bg-popover": {
+          "background-color": "hsl(var(--popover))",
+        },
+        ".bg-primary": {
+          "background-color": "hsl(var(--primary))",
+        },
+        ".bg-secondary": {
+          "background-color": "hsl(var(--secondary))",
+        },
+        ".bg-muted": {
+          "background-color": "hsl(var(--muted))",
+        },
+        ".bg-accent": {
+          "background-color": "hsl(var(--accent))",
+        },
+        ".text-foreground": {
+          color: "hsl(var(--foreground))",
+        },
+        ".text-card-foreground": {
+          color: "hsl(var(--card-foreground))",
+        },
+        ".text-popover-foreground": {
+          color: "hsl(var(--popover-foreground))",
+        },
+        ".text-primary-foreground": {
+          color: "hsl(var(--primary-foreground))",
+        },
+        ".text-secondary-foreground": {
+          color: "hsl(var(--secondary-foreground))",
+        },
+        ".text-muted-foreground": {
+          color: "hsl(var(--muted-foreground))",
+        },
+        ".text-accent-foreground": {
+          color: "hsl(var(--accent-foreground))",
+        },
+        ".border-border": {
+          "border-color": "hsl(var(--border))",
+        },
+        ".outline-ring\\/50": {
+          "outline-color": "hsl(var(--ring) / 0.1)",
+        },
+      })
+    },
+  ],
 }
 
 export default config
